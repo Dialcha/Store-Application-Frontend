6e24da4b909e46e327a53848f12d359a3d6d24df
Diego Chavarria <diego.chavarriap@gmail.com>
2020-08-30 15:26:29 -0500

    Componente header parcialmente

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Image from "../../assets/header-x1.png";
+
+const Container = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 30%;
+  left: 0px;
+  top: 80px;
+  background-image: url(${Image});
+  background-size: 100% 100%; ;
+`;
+// ajustar background-size a 20%
+
+const Title = styled.h1`
+  position: absolute;
+  height: 80px;
+  left: 9.17%;
+  right: 69.03%;
+  top: calc(50% - 80px / 2 + 118px);
+
+  font-family: Source Sans Pro;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 64px;
+  line-height: 80px;
+
+  color: black;
+`;
 
 function Header() {
-    return (
-        <div>
-            Header
-        </div>
-    )
+  return (
+    <Container>
+        <Title>Electronics</Title>
+    </Container>
+  );
 }
 
-export default Header
+export default Header;
--- src/containers/Background.jsx
+++ src/containers/Background.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 import Top from '../components/Top/Top';
 import Header from '../components/Header/Header';
 import Menu from '../components/Menu/Menu';
+import styled from "styled-components";
 
+const Container = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    max-width: 1440px;
+    max-height: 412px;
+    left: 0px;
+    top: 0px;
+
+`
 
 function Background() {
     return (
