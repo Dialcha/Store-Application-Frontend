1607f174790fc863b7526f78ddb0115392753c98
DiegoTKD94 <diego.chavarriap@gmail.com>
2021-02-03 01:27:30 -0500

    Updates

---
+++ precache-manifest.dc3c56652b3fa29a77cd370b6ebe55ed.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c11fe8de3422d149b57efb4fc43122",
+    "url": "/Store-Application-Frontend/index.html"
+  },
+  {
+    "revision": "822f7d848032298fb1d7",
+    "url": "/Store-Application-Frontend/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "bf77d92ca182253c470f",
+    "url": "/Store-Application-Frontend/static/js/2.80a831e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Store-Application-Frontend/static/js/2.80a831e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "822f7d848032298fb1d7",
+    "url": "/Store-Application-Frontend/static/js/main.7edcfe19.chunk.js"
+  },
+  {
+    "revision": "71fda91548e1682d825f",
+    "url": "/Store-Application-Frontend/static/js/runtime-main.bbd57713.js"
+  },
+  {
+    "revision": "eafbdf977fa71aaa4f363c15fc79bfc6",
+    "url": "/Store-Application-Frontend/static/media/aerolab-logo.eafbdf97.svg"
+  },
+  {
+    "revision": "35b334b79af752bbd6319e3c8830e94c",
+    "url": "/Store-Application-Frontend/static/media/buy-blue.35b334b7.svg"
+  },
+  {
+    "revision": "6c4dec8bc6e719ada7ce9ea299cf0c56",
+    "url": "/Store-Application-Frontend/static/media/buy-white.6c4dec8b.svg"
+  },
+  {
+    "revision": "da7936e61d9b61fb6a51d5fbf850d6bd",
+    "url": "/Store-Application-Frontend/static/media/coin.da7936e6.svg"
+  },
+  {
+    "revision": "f0b98264ce8c93768fbf37c263885aaa",
+    "url": "/Store-Application-Frontend/static/media/header-x1.f0b98264.png"
+  }
+]);
